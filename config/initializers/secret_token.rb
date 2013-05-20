da91af150d940c06a12ddb270a275ef11cd0b174
Avi-ec2-kyp <ubuntu@ip-10-98-171-12.ec2.internal>
2013-05-20 15:59:01 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myblog::Application.config.secret_token = '7f1e47c9f42d33f071806a62ef2690efdb3526e471f20d0f1006d04749c4142e0059f6a512a6fab1679b377d8d9ec66083294746678534a04754c7eef75d7a30'
